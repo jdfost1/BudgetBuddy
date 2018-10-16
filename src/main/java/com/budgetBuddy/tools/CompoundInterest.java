1c7c740856afe12c5337765204c44c3c8df6e944
J.D. LaFayette <jlafaye800@gmail.com>
2018-10-15 19:37:57 -0500

    Add some more constraints to compound interest fields

--- src/main/java/com/budgetBuddy/tools/CompoundInterest.java
+++ src/main/java/com/budgetBuddy/tools/CompoundInterest.java
@@ -1,19 +1,28 @@
 package com.budgetBuddy.tools;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+
+import static com.budgetBuddy.validation.CompoundInterestValidation.REQUIRED;
+import static com.budgetBuddy.validation.CompoundInterestValidation.MIN_0;
+import static com.budgetBuddy.validation.CompoundInterestValidation.MIN_1;
 
 public class CompoundInterest {
 		
-	@NotNull(message="is required")
+	@NotNull(message = REQUIRED)
+	@Min(value = 0, message = MIN_0)
 	private Double principal;
 	
-	@NotNull(message="is required")
+	@NotNull(message = REQUIRED)
+	@Min(value = 0, message = MIN_0)
 	private Double interestRate;
 	
-	@NotNull(message="is required")
+	@NotNull(message = REQUIRED)
+	@Min(value = 1, message = MIN_1)
 	private Integer compoundsPerYear;
 	
-	@NotNull(message="is required")
+	@NotNull(message = REQUIRED)
+	@Min(value = 0, message = MIN_0)
 	private Integer years;
 
 	public Double getPrincipal() {
