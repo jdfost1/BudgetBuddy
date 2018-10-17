9648c6ebb844f9ace04894bfe3a407517a581fde
Foster, Josh <jdfost1@ilstu.edu>
2018-10-16 23:05:14 -0500

    started db sql

---
+++ src/main/java/com/budgetBuddy/config/RootApplicationContextConfig.java
@@ -0,0 +1,32 @@
+package com.budgetBuddy.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@ComponentScan("com.budgetBuddy")
+public class RootApplicationContextConfig {
+
+   @Bean
+   public DataSource dataSource() {
+      EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+      EmbeddedDatabase db = builder
+         .setType(EmbeddedDatabaseType.HSQL)
+         .addScript("db/sql/users.sql")
+         
+         .build();
+      return db;
+   }
+  
+   @Bean
+   public NamedParameterJdbcTemplate getJdbcTemplate() {
+      return new NamedParameterJdbcTemplate(dataSource());
+   }
+}
