552eb7ac10fcdad57b7fb771e59e4db156b322e9
Foster, Josh <jdfost1@ilstu.edu>
2018-10-19 02:21:09 -0500

    updated user entity class

--- src/main/java/com/budgetBuddy/entities/User.java
+++ src/main/java/com/budgetBuddy/entities/User.java
@@ -1,12 +1,35 @@
 package com.budgetBuddy.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="users")
 public class User {
+	
+	@Column(name="username",length=100,nullable=false)
 	private String username;
+	
+	@Column(name="password",length=100,nullable=false)
 	private String password;
+
+	
+	//getters and setters
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
