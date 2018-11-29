db2adb9dc50829eba5fed125b7eb3a30273d40ca
Foster, Josh <jdfost1@ilstu.edu>
2018-11-28 23:55:33 -0600

    updated savings target entity class

--- src/main/java/com/budgetBuddy/entities/SavingsTarget.java
+++ src/main/java/com/budgetBuddy/entities/SavingsTarget.java
@@ -15,26 +15,68 @@ public class SavingsTarget {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-	
+
 	@Column(name = "monthly_savings_target")
 	private double monthlySavingsTarget;
-	
-	@Column(name= "savings_target")
+
+	@Column(name = "savings_target")
 	private double savingsTarget;
-	
-	@Column(name= "duration")
+
+	@Column(name = "duration")
 	private double startingDuration;
-	
-	@Column(name= "remainingBalance")
+
+	@Column(name = "remainingBalance")
 	private double startingBalance;
-	
-	@Column(name= "start_date")
+
+	@Column(name = "start_date")
 	private double startDate;
-	
-	
-	
-	
-	
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getMonthlySavingsTarget() {
+		return monthlySavingsTarget;
+	}
+
+	public void setMonthlySavingsTarget(double monthlySavingsTarget) {
+		this.monthlySavingsTarget = monthlySavingsTarget;
+	}
+
+	public double getSavingsTarget() {
+		return savingsTarget;
+	}
+
+	public void setSavingsTarget(double savingsTarget) {
+		this.savingsTarget = savingsTarget;
+	}
+
+	public double getStartingDuration() {
+		return startingDuration;
+	}
+
+	public void setStartingDuration(double startingDuration) {
+		this.startingDuration = startingDuration;
+	}
+
+	public double getStartingBalance() {
+		return startingBalance;
+	}
+
+	public void setStartingBalance(double startingBalance) {
+		this.startingBalance = startingBalance;
+	}
+
+	public double getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(double startDate) {
+		this.startDate = startDate;
+	}
+
 }
