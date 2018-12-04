fad315f0d40374bc2cf7da531921c44072d33df7
Foster, Josh <jdfost1@ilstu.edu>
2018-12-04 13:34:22 -0600

    format/cleanup code

--- src/test/java/com/budgetBuddy/test/CreateBudgetTest.java
+++ src/test/java/com/budgetBuddy/test/CreateBudgetTest.java
@@ -7,25 +7,24 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import com.budgetBuddy.model.BudgetForm;
 
+import com.budgetBuddy.model.BudgetForm;
 
 public class CreateBudgetTest {
 
 	private static Validator validator;
 	private BudgetForm testBudget;
-	
+
 	@BeforeClass
 	public static void setUpClass() {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		validator = factory.getValidator();
 	}
-	
+
 	@Before
 	public void setUp() {
 		testBudget = new BudgetForm();
@@ -36,65 +35,71 @@ public class CreateBudgetTest {
 		testBudget.setRent(750.0);
 		testBudget.setSavingsTarget(250.0);
 		testBudget.setUtilities(100.0);
-		
+
 	}
-	
+
 	@After
 	public void tearDown() {
 		testBudget = null;
 	}
-	
+
 	@Test
 	public void testBudgetFormValid() {
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertTrue(violations.isEmpty());
 	}
-	
+
 	@Test
 	public void testBudgetFormEmpty() {
 		BudgetForm reg = new BudgetForm();
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(reg);
 		assertFalse(violations.isEmpty());
 	}
-	
-	//test all fields with negative values---------------
-	
+
+	// test all fields with negative values---------------
+
 	@Test
 	public void testCarInsuranceNegative() {
 		testBudget.setCarInsurance(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testCarPaymentNegative() {
 		testBudget.setCarPayment(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testIncomeNegative() {
 		testBudget.setIncome(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testRemainingExpensesNegative() {
 		testBudget.setRemainingExpenses(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testRentNegative() {
 		testBudget.setRent(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testSavingsTargetNegative() {
 		testBudget.setSavingsTarget(-100.0);
 		Set<ConstraintViolation<BudgetForm>> violations = validator.validate(testBudget);
 		assertFalse(violations.isEmpty());
 	}
+
 	@Test
 	public void testUtilitiesNegative() {
 		testBudget.setUtilities(-100.0);
@@ -102,4 +107,3 @@ public class CreateBudgetTest {
 		assertFalse(violations.isEmpty());
 	}
 }
-
